4c3ca560a5d419a107d632a7887a2b21bd12a5d4
Kevin Ruiz <sistemas@haltda.com.co>
2024-12-26 12:54:59 -0500

    Ajuste general para empleados

--- app/Models/Empleados2011.php
+++ app/Models/Empleados2011.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Empleados2011 extends Model
 {
-    protected $table = 'empleados2011';
+    protected $table = 'empleados';
 
     protected $primaryKey = 'id';
 
